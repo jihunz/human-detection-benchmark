6527ec6c2aeb0656e2eb34bf480137229ae6c70a
jihunz <thespringsong@gmail.com>
2025-08-28 22:24:18 +0900

    feat: f1 score 산출 로직 임시 저장

--- sample/get_true_and_pred_v2.py
+++ sample/get_true_and_pred_v2.py
@@ -1,5 +1,6 @@
 import json
 import os
+from typing import Optional, List, Tuple
 
 from ultralytics import YOLO
 
@@ -45,7 +46,7 @@ def get_gt_by_img():
             if img_id not in result:
                 result[img_id] = []
 
-            if item['category_id'] == 1 and item['iscrowd'] == 0: # 군중 이미지는 예측 박스와의 매칭 복잡성의 문제로 제외
+            if item['category_id'] == 1 and item['iscrowd'] == 0:  # 군중 이미지는 예측 박스와의 매칭 복잡성의 문제로 제외
                 # if img_id == 554002: # TODO: 군중이 아니지만 사람에 대한 여러 gt가 있는 경우
                 #     print(item)
                 result[img_id].append(item['bbox'])
@@ -54,42 +55,67 @@ def get_gt_by_img():
 
 
 # 예측과 gt의 box iou 계산
-def get_iou_between_gt_pred(pred, gt):
-    result = []
+def get_iou_between_gt_pred(pred_box, gt_box):
+    xx1 = max(pred_box[0], gt_box[0])
+    yy1 = max(pred_box[1], gt_box[1])
+    xx2 = min(pred_box[2], gt_box[2])
+    yy2 = min(pred_box[3], gt_box[3])
+    w = max(0.0, xx2 - xx1)
+    h = max(0.0, yy2 - yy1)
+    inter = w * h
+    area_a = max(0.0, (pred_box[2] - pred_box[0])) * max(0.0, (pred_box[3] - pred_box[1]))
+    area_b = max(0.0, (gt_box[2] - gt_box[0])) * max(0.0, (gt_box[3] - gt_box[1]))
+    union = area_a + area_b - inter
 
-    for gt_idx in range(0, len(gt)):
-        gt_box = gt[gt_idx]
-        pred_box = pred[gt_idx]['bbox']
-
-        xx1 = max(pred_box[0], gt_box[0])
-        yy1 = max(pred_box[1], gt_box[1])
-        xx2 = min(pred_box[2], gt_box[2])
-        yy2 = min(pred_box[3], gt_box[3])
-        w = max(0.0, xx2 - xx1)
-        h = max(0.0, yy2 - yy1)
-        inter = w * h
-        area_a = max(0.0, (pred_box[2] - pred_box[0])) * max(0.0, (pred_box[3] - pred_box[1]))
-        area_b = max(0.0, (gt_box[2] - gt_box[0])) * max(0.0, (gt_box[3] - gt_box[1]))
-        union = area_a + area_b - inter
-
-        result.append(inter / union if union > 0 else 0.0)
-    return result
+    return inter / union if union > 0 else 0.0
 
 
 if __name__ == "__main__":
-    gts = get_gt_by_img()
-    preds = get_pred()
+    iou_threshold = 0.8
+    TP_result, FP_result, FN_result = 0.0, 0.0, 0.0
 
-    img_ids = set(gts.keys()) | set(preds.keys())
+    gt_source = get_gt_by_img()
+    pred_source = get_pred()
+    img_ids = set(gt_source.keys()) | set(pred_source.keys())
+
     for img_id in img_ids:
-        gt = gts.get(img_id, [])
-        pred = preds.get(img_id, [])
+        gt_list = gt_source.get(img_id, [])  # 사람 gt
+        pred_dict = pred_source.get(img_id, [])
 
-        if len(pred) == 0:  # TODO: 테스트 후 모든 db 대상 예측 시 제거
+        if len(pred_dict) == 0:  # TODO: 테스트 후 모든 db 대상 예측 시 제거
             continue
 
-        iou_list = get_iou_between_gt_pred(pred, gt)
-        print(iou_list)
+        for pi, pred_item in enumerate(pred_dict):
+            TP = FP = 0
+            best_iou = 0.0
 
-    # f1_scores = evaluate_models(COCO_ANN, MODEL_RESULT_FILES, CONF_THR, IOU_THR)
-    # plot_scores(f1_scores)
+            best_pi: Optional[int] = None
+            matched_gt = set()
+            match_list = []
+
+            if pred_item['cls'] != 0:
+                continue
+
+            for gt_item in gt_list:
+                iou = get_iou_between_gt_pred(pred_item['bbox'], gt_item)
+                if iou > best_iou:
+                    best_iou = iou
+                    best_pi = pi
+
+                if best_pi is not None and best_iou >= iou_threshold:
+                    TP += 1
+                    matched_gt.add(best_pi)
+                    match_list.append((pi, best_pi, best_iou))
+                else:
+                    FP += 1
+            FN = len(gt_source) - len(matched_gt)
+
+            TP_result += TP
+            FP_result += FP
+            FN_result += FN
+
+        precision = TP_result / (TP_result + FP_result)
+        recall = TP_result / (TP_result + FN_result)
+        f1_score = 2 * (precision * recall) / (precision + recall)
+
+        print(f1_score)
